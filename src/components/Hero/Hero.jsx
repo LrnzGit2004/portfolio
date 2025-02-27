86b3004ca78c99e15f3ad956f5c38fbcbfd9585b
Lorenzo Éric <134990909+LrnzGit2004@users.noreply.github.com>
2025-02-27 20:15:12 +0200

    commit socials 27/02/25

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -21,8 +21,8 @@ const Hero = () => {
           <span className="text-[#0EB39E] border-b border-[#0EB39E]">
             développeur frontend React
           </span>{" "}
-          passionné de nouvelles technologies, avec un sens aigue du design et de
-          l'observation. Je serais ravi de travailler avec vous pour créer de
+          passionné de nouvelles technologies, avec un sens aigue du design et
+          de l'observation. Je serais ravi de travailler avec vous pour créer de
           nouvelles choses. Alors, contactez-moi !
         </p>
 
@@ -30,17 +30,16 @@ const Hero = () => {
         <div className="flex flex-col items-center md:flex-row gap-8">
           <a
             className="flex bg-[#084843] text-white py-2 px-2 lg:px-6 rounded-lg hover:bg-[#1C6961] duration-200 gap-2 items-center w-auto"
-            
             href="https://drive.google.com/uc?export=download&id=1f3Uf7xNWItAibNoBmQcObm4-iBwLSMLf"
             download=""
           >
             Téléchargez mon CV
             <FaDownload className="bg-transparent" />
           </a>
-          <div className="socials mb-8 md:mb-0">
+          <div className="mb-8 md:mb-0">
             <Socials
               containerStyles="flex gap-6"
-              iconStyles="w-9 h-9 border border-[#1C6961] rounded-full flex items-center justify-center text-[#0EB39E] bg-transparent hover:bg-[#0BD8B6]  hover:transition-all duration-300"
+              iconStyles="w-9 h-9 border border-[#1C6961] rounded-full flex items-center justify-center text-[#0EB39E] hover:bg-[#0EB39E] hover:text-[#FFFFFF] hover:transition-all duration-300"
             />
           </div>
         </div>
--- src/components/Hero/Socials.jsx
+++ src/components/Hero/Socials.jsx
@@ -1,22 +1,33 @@
-import React from 'react'
-import { FaGithub, FaLinkedinIn, FaWhatsapp } from 'react-icons/fa'
-import { Link } from 'react-scroll'
+import React from "react";
+import { FaGithub, FaLinkedinIn, FaWhatsapp } from "react-icons/fa";
 
 const socials = [
-    {icon: <FaGithub/>, path: 'https://github.com/LrnzGit2004'},
-    {icon: <FaLinkedinIn/>, path: 'https://www.linkedin.com/in/eric-sobze'},
-    {icon: <FaWhatsapp/>, path: 'https://wa.me/237653832394'}
-]
+  {
+    icon: <FaGithub className="bg-transparent" />,
+    path: "https://github.com/LrnzGit2004",
+  },
+  {
+    icon: <FaLinkedinIn className="bg-transparent" />,
+    path: "https://www.linkedin.com/in/eric-sobze",
+  },
+  {
+    icon: <FaWhatsapp className="bg-transparent" />,
+    path: "https://wa.me/237653832394",
+  },
+];
 
-const Socials = ({containerStyles, iconStyles}) => {
+const Socials = ({ containerStyles, iconStyles }) => {
   return (
     <div className={containerStyles}>
-        {socials.map((item,index)=>{
-            return <a key={index} href={item.path} className={iconStyles} target='blank'>{item.icon}</a>
-        })}
-      
+      {socials.map((item, index) => {
+        return (
+          <a key={index} href={item.path} className={iconStyles} target="blank">
+            <p className="bg-transparent">{item.icon}</p>
+          </a>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default Socials
+export default Socials;
