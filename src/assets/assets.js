38278fec05eb094ee656697064b141db66743626
Lorenzo Éric <134990909+LrnzGit2004@users.noreply.github.com>
2025-02-27 19:53:27 +0200

    update photo et menu responsive 27/02/25

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -10,8 +10,11 @@ import asset_management_app from "./asset-management-app.png";
 import brise_services from "./brise-services.png";
 import react_school from "./react-school.png";
 import profil_portfolio from "./profil_portfolio.jpg";
-import profil_true from "./profil-true.jpg"
- 
+import profil_true from "./profil-true.jpg";
+import { FaHouseUser, FaInfoCircle } from "react-icons/fa";
+import { FaArrowRightArrowLeft } from "react-icons/fa6";
+import { SiPolymerproject } from "react-icons/si";
+
 export const assets = {
   profil,
   javascript_logo,
@@ -25,14 +28,14 @@ export const assets = {
   brise_services,
   react_school,
   profil_portfolio,
-  profil_true
+  profil_true,
 };
 
 export const LinksApp = [
-  { id: 1, name: "Acceuil", link: "hero" },
-  { id: 2, name: "Services", link: "service" },
-  { id: 3, name: "Projets", link: "project" },
-  { id: 4, name: "A propos", link: "about" },
+  { id: 1, name: "Acceuil", link: "hero", icon: FaHouseUser },
+  { id: 2, name: "Services", link: "service", icon: FaArrowRightArrowLeft },
+  { id: 3, name: "Projets", link: "project", icon: SiPolymerproject },
+  { id: 4, name: "A propos", link: "about", icon: FaInfoCircle },
 ];
 
 export const education = [
@@ -40,36 +43,36 @@ export const education = [
     institut: "Online Course GreatStack",
     diplome: "Frontend skills",
     duration: "juin 2024 - octobre 2024",
-    type: "Certification"
+    type: "Certification",
   },
   {
     institut: "WenSchool",
     diplome: "Developpeur Frontend & Point focal",
     duration: "juillet 2024 - now",
-    type: "Exp. professionnelle"
+    type: "Exp. professionnelle",
   },
   {
     institut: "IUT Ngaoundéré",
     diplome: "Génie logiciel",
     duration: "2022 - now",
-    type: "Diplome"
+    type: "Diplome",
   },
   {
     institut: "KPDC",
     diplome: "Stagiaire Génie logiciel",
     duration: "mars 2024 - juin 2024",
-    type: "Attestation"
+    type: "Attestation",
   },
   {
     institut: "Online Sololearn",
     diplome: "HTML, CSS, Js",
     duration: "2022",
-    type: "Certification"
+    type: "Certification",
   },
   {
     institut: "Lycée Bilingue de Dombè",
     diplome: "Baccalauréat Scientifique",
     duration: "2021",
-    type: "Diplome"
+    type: "Diplome",
   },
 ];
--- src/components/Header/NavBar.jsx
+++ src/components/Header/NavBar.jsx
@@ -41,13 +41,13 @@ const NavBar = () => {
           onClick={() => setVisible(true)}
           src={assets.menu}
           alt=""
-          className="w-10 cursor-pointer lg:hidden"
+          className="w-10 cursor-pointer rotate-[180deg] lg:hidden "
         />
       </div>
 
       {/* sideBar pour les petits écrans */}
       <div
-        className={`absolute top-0 right-0 bottom-0 overflow-hidden shadow-sm transition-all z-10 ${
+        className={`absolute top-5 right-0 h-screen overflow-hidden shadow-sm transition-all z-10 ${
           visible ? "w-[90%]" : "w-0"
         }`}
       >
@@ -56,28 +56,24 @@ const NavBar = () => {
             className="flex items-center gap-4 p-3 cursor-pointer "
             onClick={() => setVisible(false)}
           >
-            <FaArrowCircleRight className="bg-[#111C1B] w-10 h-10 rounded-full" />
+            <FaArrowCircleRight className="bg-[#111C1B] w-8 h-8 rounded-full" />
           </div>
           {/* //on map sur les liens générés dans la variable LinksApp de assets */}
 
           {LinksApp.map((link) => (
-            <li
+            <Link
               key={link.id}
-              className="list-none py-2 border-b-2 w-full hover:bg-[#1C6961] cursor-pointer duration-200"
+              to={link.link}
+              smooth={true}
+              duration={500}
+              className="font-medium text-lg list-none p-2  w-full hover:bg-[#1C6961] cursor-pointer duration-200 bg-transparent border-b-2 ml-4 flex items-center gap-3"
+              onClick={() => setVisible(false)}
             >
-              <Link
-                to={link.link}
-                smooth={true}
-                duration={500}
-                className="font-medium text-lg bg-transparent ml-4"
-                onClick={() => setVisible(false)}
-              >
-                {link.name}
-              </Link>
-            </li>
+              {link.icon && <link.icon className="ml-2 bg-transparent" />}
+              {link.name}
+            </Link>
           ))}
-          <Button className={`!w-[35%] mt-4`}>
-            {" "}
+          <Button className={`!w-[30%] mt-4 text-center`}>
             <Link
               to="contact"
               smooth={true}
@@ -85,7 +81,7 @@ const NavBar = () => {
               className="bg-transparent"
             >
               Contactez-moi
-            </Link>{" "}
+            </Link>
           </Button>
         </div>
       </div>
