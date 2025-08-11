bbfc8f157f6ff68e41c0b6217236d7b738cabecd
Eric Lorryl SOBZE <134990909+LrnzGit2004@users.noreply.github.com>
2025-08-11 09:28:35 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='footer'>
-      <p>© 2024. Réalisé par Eric Lorenzo SOBZE. Tous droits réservés.</p> {/* Alt+0169 */}
+      <p> Copyright © {new Date().getFullYear()} Réalisé par Eric Lorry SOBZE. Tous droits réservés.</p> {/* Alt+0169 */}
         <ul>
             <li>Termes et conditions</li>{}
             <li>Politique de confidentialité</li>
